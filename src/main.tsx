bf63e23c53c59039df77570129b1247cb5a814ac
Scott Mills <scott@sgmstudios.ca>
2026-07-21 13:47:36 -0700

    Add optional Sentry React init via VITE_SENTRY_DSN

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initSentry } from "./sentry";
+
+initSentry();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
